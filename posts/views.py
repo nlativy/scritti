7f8e3e27de3afe34cd38b43bd3c62b13dd2a434e
Nicholas Lativy <nlativy{a.}gmail.com>
2009-03-17 09:41:46 +0000

    Add urls for index and individual post. Add stub views for posts.

--- posts/views.py
+++ posts/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world! You are at the post index.")
+
+def detail(request, post_slug):
+    return HttpResponse("Hello, you're looking at the post with slug: %s." % post_slug)
