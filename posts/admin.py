b0b743f5bc639b42a4dc78d5950819a408c08d2e
Nicholas Lativy <nlativy{a.}gmail.com>
2009-03-17 09:28:05 +0000

    Enable admin interface, add posts to admin interface.

---
+++ posts/admin.py
@@ -0,0 +1,11 @@
+from scritti.posts.models import Post
+from django.contrib import admin
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+            ('Post',                {'fields': ['subject', 'body']}),
+            ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ]
+    list_display = ('subject', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
