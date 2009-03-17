3d33203c5883d936d8cd9aff9419a7f026659dbf
Nicholas Lativy <nlativy{a.}gmail.com>
2009-03-17 09:06:30 +0000

    Modify settings to use sqlite3 database. Add SITE_ROOT variable set dynamically so all paths can be given relative to project root.

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 # Django settings for scritti project.
 
+import os
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,8 +13,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = os.path.join(SITE_ROOT, 'test_database.db')    # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
