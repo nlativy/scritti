35551adb8317bdff1cf6035a9cfcf2ae9a074b59
Nicholas Lativy <nlativy{a.}gmail.com>
2009-03-18 09:39:13 +0000

    Add sample CSS file and make use of it in index.html. Update urls.py so that django will serve static files in debug mode, this will be done by apache in production.

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+import os
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,3 +19,9 @@ urlpatterns = patterns('',
     (r'^$', 'scritti.posts.views.index'),
     url(r'^articles/(?P<post_slug>.*)/$', 'scritti.posts.views.detail', name='post_view'),
 )
+
+if settings.DEBUG:
+    media_path = os.path.join(settings.SITE_ROOT, 'site_media/')
+    urlpatterns += patterns('',
+            (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': media_path}),
+        )
