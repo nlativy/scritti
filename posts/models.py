aa7d75a6b39fabd0f0b3c14cb9c9a056f448b1d4
Nicholas Lativy <nlativy{a.}gmail.com>
2009-03-18 13:46:45 +0000

    Start using markdown for post body. Add an additional database column for body_html so that conversion to html is only performed once.

--- posts/models.py
+++ posts/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.db.models import permalink
+from markdown import markdown
 import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
     slug = models.SlugField(max_length=128, unique=True)
     body = models.TextField()
+    body_html = models.TextField(editable=False, blank=True)
     pub_date = models.DateTimeField('publication date')
 
     def beforeSave(self):
         if (not self.pub_date):
             self.pub_date = datetime.datetime.now()
+
+        self.body_html = markdown(self.body)
 
     def save(self):
         self.beforeSave()
